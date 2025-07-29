ea4e7521846aa4eda43819ac20c809cccf0dbb45
Eddie Zhang <jjzhang2000@hotmail.com>
2025-07-29 22:22:27 +0800

    20250729 LibraryPage, TextButton

--- lib/Library/library_page.dart
+++ lib/Library/library_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:bilingual_reader/Setting/settings.dart';
 import 'package:flutter/material.dart';
 
@@ -34,8 +36,31 @@ class _LibraryPageState extends State<LibraryPage> {
         itemCount: bookCount,
         itemBuilder: (BuildContext context, int index) {
           return Container(
-            height: 50,
-            child: Text('Entry ${library.books[index].title}'),
+            padding: const EdgeInsets.all(5.0),
+            height: 100,
+            child: TextButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Image.file(
+                    File(library.books[index].coverImageFilename),
+                    errorBuilder: (context, error, stackTrace) {
+                      return const Icon(Icons.book, size: 50);
+                    },
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    library.books[index].title,
+                    style: const TextStyle(fontSize: 18),
+                    overflow: TextOverflow.fade,
+                  ),
+                ],
+              ),
+            ),
           );
         },
       ),
